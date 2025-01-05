8b3dbcfae911adaf6f82a38c13b7679685bba282
amandine <amandine@monkey>
2025-01-05 20:14:40 +0100

    Modifications Amandine deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [viteStaticCopy({
+      targets: [
+        {
+          src: 'data/**/*', // Tous les fichiers dans le dossier data
+          dest: 'data',    // Destination dans le dossier dist
+        },
+      ],
+    }),react()],
   base: "/cv/",
   optimizeDeps: {
     exclude: ['lucide-react'],
